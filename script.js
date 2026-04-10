7f3264f2a56117b4bf71b88b64eec6ad95698152
NitroTGM <tiaanmal123@gmail.com>
2026-04-10 17:17:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// DARK / LIGHT MODE
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    toggle.textContent = "☀️";
+  } else {
+    toggle.textContent = "🌙";
+  }
+});
+
+// SCROLL ANIMATIONS
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in").forEach(el => {
+  observer.observe(el);
+});
